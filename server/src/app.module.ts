49c4d654cb942d2af7f0289112437846b6f5b69d
mullersoft <mulerselinger@gmail.com>
2024-11-18 12:55:06 +0300

    preparation for deployment

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,16 +1,18 @@
 // src/app.module.ts
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose'; // Import MongooseModule
-import { UsersModule } from './modules/users/users.module'; // Import UsersModule
-import { ProjectsModule } from './modules/projects/projects.module'; // Import ProjectsModule
-import { ContactModule } from './modules/contact/contact.module'; // Import ContactModule
+import { ConfigModule } from '@nestjs/config'; // Import ConfigModule
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './modules/users/users.module';
+import { ProjectsModule } from './modules/projects/projects.module';
+import { ContactModule } from './modules/contact/contact.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/your-database-name'), // MongoDB connection
-    UsersModule, // Import UsersModule
-    ProjectsModule, // Import ProjectsModule
-    ContactModule, // Import ContactModule
+    ConfigModule.forRoot(), // Initialize ConfigModule to load .env variables
+    MongooseModule.forRoot(process.env.MONGO_URI), // Use MONGO_URI from .env
+    UsersModule,
+    ProjectsModule,
+    ContactModule,
   ],
   controllers: [],
   providers: [],
