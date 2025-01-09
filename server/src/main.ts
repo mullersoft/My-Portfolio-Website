652af5621021b12223ac207c5fce14312589fc6e
mullersoft <mulerselinger@gmail.com>
2025-01-09 17:53:22 +0300

    ff

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,28 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
-
 dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const allowedOrigins = [
-    // 'http://localhost:3000/', // Local development
-    'https://aesthetic-stroopwafel-42b2f3.netlify.app/', // Deployed frontend
+    'https://aesthetic-stroopwafel-42b2f3.netlify.app', // Deployed frontend
   ];
 
   app.enableCors({
     origin: (origin, callback) => {
-      // Allow the origin or when no origin is present (e.g., in a curl request)
       if (!origin || allowedOrigins.includes(origin)) {
         callback(null, true);
       } else {
         callback(new Error('Not allowed by CORS'));
       }
     },
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Define allowed methods if needed
-    allowedHeaders: 'Content-Type, Accept', // Set allowed headers
+    methods: 'GET, POST, PUT, DELETE, PATCH',  // Ensure allowed methods
+    allowedHeaders: 'Content-Type, Accept',  // Define allowed headers
+    credentials: true,  // If you're using credentials like cookies
   });
 
   await app.listen(5000);
