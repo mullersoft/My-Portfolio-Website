920c7661bf0079025b7778e2e6d6265eddf11ad9
mullersoft <mulerselinger@gmail.com>
2025-01-10 20:48:52 +0300

    core conflict

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,20 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as cors from 'cors'; // Import cors
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Correct CORS configuration (with or without trailing slash)
-  app.use(
-    cors({
-      origin: 'https://adorable-kelpie-19779b.netlify.app', // Make sure it matches exactly with the frontend URL (no trailing slash)
-      methods: ['GET', 'POST', 'PUT', 'DELETE'],
-      allowedHeaders: ['Content-Type', 'Authorization'],
-    }),
-  );
+  const allowedOrigins = [
+    // 'http://localhost:3000/', // Local development
+    'https://aesthetic-stroopwafel-42b2f3.netlify.app', // Deployed frontend
+  ];
 
-  await app.listen(5000); // Ensure this is the correct port your backend is running on
+  app.enableCors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+  });
+
+  await app.listen(5000);
 }
 
 bootstrap();
