b20053403dee78fd358a6989dcfadb36b0798335
mullersoft <mulerselinger@gmail.com>
2025-01-10 18:52:15 +0300

    core conflict

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,27 +1,35 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
+
 dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const allowedOrigins = [
-    // 'http://localhost:3000/', // Local development
+    'http://localhost:3000', // Local development
     'https://aesthetic-stroopwafel-42b2f3.netlify.app', // Deployed frontend
   ];
 
   app.enableCors({
     origin: (origin, callback) => {
-      if (!origin || allowedOrigins.includes(origin)) {
+      console.log('Origin:', origin); // Log incoming request origins
+      if (
+        !origin ||
+        allowedOrigins.some((allowedOrigin) => origin.startsWith(allowedOrigin))
+      ) {
         callback(null, true);
       } else {
+        console.error('Blocked by CORS:', origin); // Log blocked origins
         callback(new Error('Not allowed by CORS'));
       }
     },
   });
 
-  await app.listen(5000);
+  const PORT = process.env.PORT || 5000; // Use environment variable for the port
+  await app.listen(PORT);
+  console.log(`Server is running on http://localhost:${PORT}`);
 }
 
 bootstrap();
