38c764e87c04719728bcc12999b0987aadf67e9a
mullersoft <mulerselinger@gmail.com>
2025-01-10 20:37:59 +0300

    core conflict

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3000', // Frontend URL
+    origin: 'https://aesthetic-stroopwafel-42b2f3.netlify.app/', // Frontend URL
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   });
   await app.listen(3000);
