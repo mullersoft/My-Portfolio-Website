a00b93bc21859b40f8c65c9b4a0a715d54ed8e26
mullersoft <mulerselinger@gmail.com>
2025-01-10 20:33:37 +0300

    core conflict

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,26 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as dotenv from 'dotenv';
-dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  const allowedOrigins = [
-    'https://aesthetic-stroopwafel-42b2f3.netlify.app/', // Replace with your actual frontend URL
-  ];
-
   app.enableCors({
-    origin: (origin, callback) => {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
+    origin: 'http://localhost:3000', // Frontend URL
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   });
-
-  await app.listen(5000);
+  await app.listen(3000);
 }
-
 bootstrap();
