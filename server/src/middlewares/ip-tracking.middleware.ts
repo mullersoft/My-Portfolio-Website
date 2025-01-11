b3b8746f4427524ae0afee0da6743f965fcd348c
mullersoft <mulerselinger@gmail.com>
2025-01-11 12:32:59 +0300

    Revert "k"
    This reverts commit a466faeb3790a1d70c191bc84f70ed1ff05c718e.
    
    :wq
    
    mybranch :wq

--- server/src/middlewares/ip-tracking.middleware.ts
+++ server/src/middlewares/ip-tracking.middleware.ts
@@ -15,15 +15,12 @@ export class IpTrackingMiddleware implements NestMiddleware {
     try {
       const response = await axios.get(`http://ip-api.com/json/${ipAddress}`);
       const { country, city, status, query } = response.data;
-
+      
       if (status !== 'success' || !country || !city) {
-        console.warn(
-          `Incomplete or failed location data for IP ${query}:`,
-          response.data,
-        );
+        console.warn(`Incomplete or failed location data for IP ${query}:`, response.data);
         return 'Unknown Location';
       }
-
+      
       return `${city}, ${country}`;
     } catch (error) {
       console.error('Error fetching geo-location:', error.message);
@@ -38,33 +35,21 @@ export class IpTrackingMiddleware implements NestMiddleware {
         text: message,
       });
     } catch (error) {
-      console.error(
-        'Error sending notification to Telegram:',
-        error.response?.data || error.message,
-      );
+      console.error('Error sending notification to Telegram:', error.response?.data || error.message);
     }
   }
 
   async use(req: Request, res: Response, next: NextFunction) {
-    let ipAddress = (
-      (req.headers['x-forwarded-for'] as string) ||
-      req.connection.remoteAddress ||
-      req.ip ||
-      ''
-    )
-      .split(',')[0]
-      .trim();
+    let ipAddress = req.headers['x-forwarded-for'] as string || req.connection.remoteAddress || req.ip;
+    ipAddress = Array.isArray(ipAddress) ? ipAddress[0] : ipAddress;
 
-    if (ipAddress.startsWith('::1') || ipAddress === '127.0.0.1') {
+    if (ipAddress === '::1' || ipAddress === '127.0.0.1') {
       ipAddress = '127.0.0.1'; // Normalize localhost IP
     }
 
     const userAgent = req.headers['user-agent'] || 'Unknown User-Agent';
     const requestedUrl = req.originalUrl;
-    const location =
-      ipAddress === '127.0.0.1'
-        ? 'Localhost'
-        : await this.getGeoLocation(ipAddress);
+    const location = ipAddress === '127.0.0.1' ? 'Localhost' : await this.getGeoLocation(ipAddress);
 
     const notificationMessage = `🌐 New Website Visit:\n\nIP Address: ${ipAddress}\nLocation: ${location}\nPage: ${requestedUrl}\nUser-Agent: ${userAgent}`;
 
